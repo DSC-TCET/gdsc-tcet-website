5b7bd5f7ee7b8043f635358b3cebee20f09f0748
Sanskar Mishra <95489547+whythoSanskar@users.noreply.github.com>
2022-10-02 09:15:19 +0530

    Update data.js

--- src/Components/Team/data.js
+++ src/Components/Team/data.js
@@ -105,7 +105,7 @@ export const technical = [
   {
     name: 'Sanskar Mishra',
     post: 'Technical Member',
-    imageurl: 'https://raw.githubusercontent.com/whythoSanskar/Assets/main/image/ay22_23/team/SanskarMishra_Technical%20Member.jpg',
+    imageurl: "https://raw.githubusercontent.com/DSC-TCET/Assets/main/image/ay22_23/team/SanskarMishra_Technical Member.jpg",
     githuburl: 'https://github.com/whythoSanskar',
     linkedinurl: 'https://www.linkedin.com/in/sanskar-mishra0',
     twitterUrl: 'https://twitter.com/whythoSanskar?t=8JVtfMLXTYIwXox1IBW1Jw&s=09',
