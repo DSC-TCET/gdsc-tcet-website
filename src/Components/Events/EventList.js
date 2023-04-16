8682b89e506d8b87f4bd3a6afbb47ca371cb564a
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-04-16 18:59:18 +0530

    Added past events

--- src/Components/Events/EventList.js
+++ src/Components/Events/EventList.js
@@ -3,6 +3,234 @@
 const EventList = [
   // -------------------------- 2022-23 Event Data --------------------------
   {
+    id: 25,
+    title: "Magic of Power BI",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-magic-of-power-bi/",
+    date: "Mar 9 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/power.png",
+    description:
+      "In this session, we will explore the magic of Power BI and how it can help organizations harness the power of their data. Whether you are a student, or an analyst, understanding Power BI is a must-have skill in today's competitive business world.",
+    agenda1: "Introduction to BI and its importance in today's business world",
+    agenda2: "Understanding the role of BI in Decision Making and Problem Solving",
+    agenda3: "Exploring various BI tools",
+    agenda4: "Overview of Power BI",
+    agenda5: "Resources for further learning",
+    agenda6: "QnA",
+  },
+  {
+    id: 24,
+    title: "Dive into Tensorflow and Kaggle",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-dive-into-tensorflow-and-kaggle/",
+    date: "Apr 5 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/ML_TWZKawG.png",
+    description:
+      "We will be exploring in this session from introduction to Hands on Tensorflow model building 👨‍💻 Tensorflow, developed by Google, is a powerful open-source machine learning library. We will also be focusing on Kaggle, one of the most popular platforms for machine learning competitions 🏆",
+    agenda1: "Introduction to Tensorflow basics",
+    agenda2: "Hands-on with Tensorflow model",
+    agenda3: "Overview of Kaggle Platform",
+    agenda4: "QnA",
+  },
+  {
+    id: 23,
+    title: "Breaking Down Machine Learning & Building Classification App",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-breaking-down-machine-learning-building-classification-app/",
+    date: "Mar 25 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/ML_TWZKawG.png",
+    description:
+      "Whether you're a beginner looking to get started or an experienced professional seeking to refresh your skills and explore new tools 🛠️, this workshop has something for everyone. We'll break down the fundamentals of machine learning, explore its real-world applications, and take a hands-on approach to building a classification ML model with the popular low-code tool. So get ready to unlock the power of machine learning and take your skills to the next level in this engaging and practical workshop 👨‍💻",
+    agenda1: "Introduction to ML",
+    agenda2: "Understanding AI vs ML vs AGI",
+    agenda3: "Why there is a need to learn ML",
+    agenda4: "Categories in ML",
+    agenda5: "Real Life Applications",
+    agenda6: "QNA",
+    youtubeLink: "https://youtu.be/mhL8QEWo2j4",
+  },
+  {
+    id: 22,
+    title: "Flutter Forward Extended TCET: Coding a live project",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-flutter-forward-extended-tcet-coding-a-live-project-1/",
+    date: "Mar 20 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/fLUTTER_8gd7SPw.png",
+    description:
+      "Flutter lets you build apps for mobile, web, and desktop, all with one codebase and with Moreficent, you can even run your apps in the cloud and access them through your browser, making it easier than ever to develop and test your apps 📱 In this session, we'll tap into Flutter and its scope, learn about the amazing features of Moreficent, and get hands-on experience building applications using Flutter 👨‍💻 So get ready to unleash your creativity and build apps that will take the world by storm!",
+    agenda1: "Tap into Flutter and it's Scope",
+    agenda2: "Understand how to run applications in the cloud using Moreficent",
+    agenda3: "Get started with Dart programming",
+    agenda4: "Learn best practices for building applications with Flutter",
+    agenda5: "QnA",
+  },
+  {
+    id: 21,
+    title: "Google Solution Challenge: Design Day",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-google-solution-challenge-design-day/",
+    date: "Mar 18 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/solution%20challenge1.jfif",
+    description:
+      "Design Day is a one day event that takes place as part of the Solution Challenge. During the event, participants are invited to collaborate with each other and with Google mentors to develop their proposed solutions 💡 The event provides an opportunity for participants to refine their ideas, receive feedback from experts, and learn more about Google's technologies and design principles 👨‍💻",
+    agenda1: "All participants must respect their assigned mentor, event organizers, and other teams at all times",
+    agenda2: "Participants must maintain a professional attitude throughout the event",
+    agenda3: "Any form of cheating or plagiarism will result in immediate disqualification.",
+  },
+  {
+    id: 20,
+    title: "Flutter Forward Extended TCET: Getting started with Flutter and Dart",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-flutter-forward-extended-tcet-getting-started-with-flutter-and-dart/",
+    date: "Mar 9 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/fLUTTER_8gd7SPw.png",
+    description:
+      "Flutter is a powerful toolkit that lets you create stunning UIs and animations, while Dart is a versatile programming language that makes coding a breeze👨‍💻 Whether you're a beginner or an experienced developer, this session is designed to help you tap into the potential of Flutter and its scope, experience its simplicity and ease-of-life, and get started with Dart programming‎ ‍‎‍💻 So buckle up and get ready to take your app development skills to the next level!",
+    agenda1: "Tap into Flutter and it's Scope",
+    agenda2: "Experience its Simplicity & Ease-of-Life",
+    agenda3: "Get started with Dart programming",
+    agenda4: "QnA",
+  },
+  {
+    id: 19,
+    title: "Postman API Workshop",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-postman-api-workshop/",
+    date: " Feb 17 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/postman%20logo.png",
+    description:
+      "You will learn about APIs in this Workshop, including what they are and their significance in the technological world. Additionally, we'll work with Postman and get our hands dirty.",
+    agenda1: "Explore API's",
+    agenda2: "Hands-on with API",
+    agenda3: "Understanding the benefits by implementing",
+    agenda4: "To certify students with the Postman Student Expert certification & advocate the use of Postman",
+    agenda5: "QnA",
+  },
+  {
+    id: 18,
+    title: "Ideathon - Google Solution Challenge",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-ideathon-google-solution-challenge/",
+    date: " Feb 11 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/Solution%20Challenge_PuSuiTP.png",
+    description:
+      "An ideathon is a short, intensive, workshop-like experience for students to address some of the most pressing challenges of our time. Participants work in teams and use design thinking and innovative learning practices to ideate and collaborate on possible solutions.",
+    agenda1: "About Google Cloud Program",
+    agenda2: "Tracks in Program and Tour to Qwiklabs",
+    agenda3: "Practices needed to be followed with Basics Cloud Computing",
+    agenda4: "Understanding the need for GCP with Various Features of GCP",
+    agenda5: "QnA",
+    youtubeLink: "https://www.youtube.com/embed/Eiy9kQTgQSo",
+  },
+  {
+    id: 17,
+    title: "Roadmap to CyberSecurity",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-roadmap-to-cybersecurity/",
+    date: "Feb 4 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/Cybersec%20logo.png",
+    description:
+      "Cybersecurity is the process of protecting and recovering systems, networks, devices, and programs from the attacks. In this session, we would be focusing on exploring the various components and fields involved in cybersecurity. Also, we will be exploring the roadmap to get started with cybersecurity 👨‍💻.",
+    agenda1: "Intro to cybersecurity",
+    agenda2: "Pre-requisite",
+    agenda3: "Types - Red Teaming, Blue Teaming, etc. and comparison",
+    agenda4: "System Requirements for cybersecurity",
+    agenda5: "Job Opportunities",
+    agenda6: "Resources",
+    agenda7: "Important Certification",
+    agenda8: "QnA",
+    youtubeLink: "https://youtu.be/DEVnV_e5JVg",
+  },
+  {
+    id: 16,
+    title: "Info Session on Google Solution Challenge",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-info-session-on-google-solution-challenge/",
+    date: "Jan 17, 2023",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/Challenge.jpg",
+    description:
+      "Students are invited to submit solutions to local community problems using one or more Google products and platforms as part of Google's Solution Challenge annually.",
+    agenda1: "Intro to Solution Challenge",
+    agenda2: "Understanding UN 17 Sustainable Development Goals",
+    agenda3: "TimeLine and Steps of Challenge",
+    agenda4: "Prizes of Challenge",
+    agenda5: "Judging Criteria",
+    agenda6: "Submission Criteria",
+    agenda7: "Exploring past winners solution",
+    agenda8: "FAQ",
+    agenda9: "QnA",
+    youtubeLink: "https://youtu.be/6rZkGKC-qXM",
+  },
+  {
+    id: 15,
+    title: "Info Session to Google Cloud Campign ☁️",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-intro-to-google-cloud-campaign/",
+    date: "14 November 2022",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/Cloud_RIOxw4v.png",
+    description:
+      "Google Cloud Campaign Program will provide you with an opportunity to kickstart your career in the cloud. The program will introduce you to various Google Cloud Platform (GCP) services and Hands-on experience with the same.",
+    agenda1: "About Google Cloud Program",
+    agenda2: "Tracks in Program and Tour to Qwiklabs",
+    agenda3: "Practices needed to be followed with Basics Cloud Computing",
+    agenda4: "Understanding the need for GCP with Various Features of GCP",
+    agenda5: "QnA",
+    youtubeLink: "https://www.youtube.com/embed/Eiy9kQTgQSo",
+  },
+  {
+    id: 14,
+    title: "World of AutoML in GCP",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-world-of-automl-in-gcp/",
+    date: "Dec 24, 2022",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/ml_0jqhkZk.png",
+    description:
+      "Google Cloud Campaign Program will provide you with an opportunity to kickstart your career in the cloud. The program will introduce you to various Google Cloud Platform (GCP) services and Hands-on experience with the same.",
+    agenda1: "Basics of Cloud Machine Learning",
+    agenda2: "Machine Learning Refresher",
+    agenda3: "Types of GCP Machine Learning",
+    agenda4: "GCP ML Clients",
+    agenda5: "Machine Learning API Services",
+    agenda6: "Dive into the AutoML",
+    agenda7: "Building AutoML Vision Model on GCP",
+    agenda8: "QnA",
+    youtubeLink: "https://youtu.be/xr5bHV2syhk",
+  },
+  {
+    id: 13,
+    title: "Info on Outreachy",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-intro-to-google-cloud-campaign/",
+    date: "Dec 21, 2022",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/Outreachy.png",
+    description:
+      "We will discuss the opportunity into open source and how you can get into it by walking you through the application process of Outreachy and answering your questions, at the Info on Outreachy Session.",
+    agenda1: "About Google Cloud Program",
+    agenda1: "Info about Outreachy",
+    agenda2: "Discusssion on it's application process",
+    agenda3: "QnA",
+    youtubeLink: "https://www.youtube.com/live/8jux__2yOsk?feature=share",
+  },
+  {
+    id: 12,
+    title: "Operationalizing ML Model on Cloud ☁️",
+    url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-operationalizing-ml-model-on-cloud/",
+    date: "Dec 17, 2022",
+    imageurl:
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_3,f_auto,g_center,h_175,q_auto:good,w_175/v1/gcs/platform-data-dsc/events/google%20cloud_XhaK73o.png",
+    description:
+      "Google Cloud Campaign Program will provide you with an opportunity to kickstart your career in the cloud. The program will introduce you to various Google Cloud Platform (GCP) services and Hands-on experience with the same.",
+    agenda1: "Why ML into the Cloud",
+    agenda2: "Building ML Model",
+    agenda3: "Deploying ML Model",
+    agenda4: "Inferencing: Online & Batch",
+    agenda5: "Building Pipelines",
+    agenda6: "Canary Testing",
+    agenda7: "Artifact Tracking",
+    agenda8: "How do we operationalize model",
+    agenda9: "Q&A",
+  },
+  {
     id: 11,
     title: "Info Session to Google Cloud Campign ☁️",
     url: "https://gdsc.community.dev/events/details/developer-student-clubs-thakur-college-of-engineering-and-technology-mumbai-presents-intro-to-google-cloud-campaign/",
@@ -16,7 +244,7 @@ const EventList = [
     agenda3: "Practices needed to be followed with Basics Cloud Computing",
     agenda4: "Understanding the need for GCP with Various Features of GCP",
     agenda5: "QnA",
-    youtubeLink : "https://www.youtube.com/embed/Eiy9kQTgQSo"
+    youtubeLink: "https://www.youtube.com/embed/Eiy9kQTgQSo",
   },
   {
     id: 10,
@@ -31,7 +259,7 @@ const EventList = [
     agenda2: "About Cloud Campaign and Opportunities to learn",
     agenda3: "Registration Details and Necessary Requirements for Registration",
     agenda4: "QnA with Gist of our Future Cloud Event",
-    youtubeLink : "https://www.youtube.com/embed/z7Kmd7I-CRI"
+    youtubeLink: "https://www.youtube.com/embed/z7Kmd7I-CRI",
   },
   {
     id: 9,
@@ -46,7 +274,7 @@ const EventList = [
     agenda2: "Requests and Responses",
     agenda3: "Follow-up resources",
     agenda4: "QnA",
-    youtubeLink : "https://www.youtube.com/embed/fc1QCj_Rl3w"
+    youtubeLink: "https://www.youtube.com/embed/fc1QCj_Rl3w",
   },
   {
     id: 8,
