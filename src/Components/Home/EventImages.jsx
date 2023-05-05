65495de234f3e9c5c7a607707b82c7b1f8c5d424
Sanskar Mishra <95489547+whythoSanskar@users.noreply.github.com>
2023-05-06 00:22:02 +0530

    Update EventImages.jsx

--- src/Components/Home/EventImages.jsx
+++ src/Components/Home/EventImages.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import './Eventimages.css';
@@ -7,48 +9,51 @@ import image3 from '../Images/img3.jpg';
 
 function EventImages() {
   return (
-    <Carousel>
+    <Carousel
+    autoPlay={true}
+    interval={5000}
+    controls={false}
+    indicators={false}
+    >
       <Carousel.Item>
         <img
           src={image1}
-          width="70%"
-          height="30%"
+          width="60%"
+          height="auto"
           style={{ margin: 'auto' }}
           alt="First slide"
         />
         <Carousel.Caption>
           <h3>Event 1</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           src={image2}
-          width="70%"
-          height="30%"
+          width="60%"
+          height="auto"
           style={{ margin: 'auto' }}
           alt="Second slide"
         />
 
         <Carousel.Caption>
           <h3>Event 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           src={image3}
-          width="70%"
-          height="30%"
+          width="60%"
+          height="auto"
           style={{ margin: 'auto' }}
           alt="Third slide"
         />
 
         <Carousel.Caption>
           <h3>Event 3</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
